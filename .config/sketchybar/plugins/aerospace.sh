c43158d919bb8b3530bcd33bc067d092ac2c9cfa
isromero <ismaromeroortega@gmail.com>
2025-09-03 09:20:43 +0200

    oops prev commit

--- .config/sketchybar/plugins/aerospace.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set $NAME background.color=$ITEM_BG_COLOR label.shadow.drawing=on icon.shadow.drawing=off background.border_width=2
-else
-  sketchybar --set $NAME background.color=$ITEM_BG_COLOR label.shadow.drawing=off icon.shadow.drawing=off background.border_width=0
-fi
